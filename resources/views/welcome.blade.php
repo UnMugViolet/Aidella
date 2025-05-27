f9e92a383a4567d4027037e9ba823adb3fa5c66e
UnMugViolet <jaguinpaul@gmail.com>
2025-05-27 15:39:34 +0200

    Getting started with backpack

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,16 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Aidella</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    <body>
         <div id="app"></div>
     </body>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </html>
