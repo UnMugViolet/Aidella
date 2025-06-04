e2ef8173078ac9c24fb0da491e15577373d5e711
unmugviolet <jaguinpaul@gmail.com>
2025-06-04 16:57:03 +0200

    Fixing up the issue on the single dog displaying another post

--- app/Http/Controllers/SingleDogController.php
+++ app/Http/Controllers/SingleDogController.php
@@ -11,12 +11,13 @@ class SingleDogController extends Controller
 
     public function show($slug)
     {
-        $blogPost = BlogPost::with('dogRace')
-                        ->where('slug', $slug)
-                        ->with('pictures')
-                        ->firstOrFail();
+        $blogPost = BlogPost::with(['dogRace', 'pictures'])
+            ->where('slug', $slug)
+            ->whereNotNull('dog_race_id')
+            ->firstOrFail();
+
         $dogRaces = DogRace::with('blogPost')->get();
-    
+
         return view('single_dog', [
             'blogPost' => $blogPost,
             'dogRaces' => $dogRaces,
