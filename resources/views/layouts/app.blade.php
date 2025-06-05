03fa142e1d5378cc9e40db628fff464e8def05cc
unmugviolet <jaguinpaul@gmail.com>
2025-06-05 17:27:48 +0200

    📈 Added matomo tracking

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,4 +48,12 @@
             window.pageData = @yield('data', '{}');
         </script>
     </body>
+    <script>
+        var _mtm = window._mtm = window._mtm || [];
+        _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+        (function() {
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src='https://matomo.rocketegg.systems/js/container_82A85JUj.js'; s.parentNode.insertBefore(g,s);
+        })();
+    </script>
 </html>
