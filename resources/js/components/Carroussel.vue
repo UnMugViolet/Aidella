01c332b9bcb0c1620254ca56f23c10bfb955479c
unmugviolet <jaguinpaul@gmail.com>
2025-06-05 12:55:00 +0200

    Added the carroussel

---
+++ resources/js/components/Carroussel.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="w-full md:w-1/2 flex flex-col items-center">
+        <div class="relative w-full">
+            <div
+				v-if="pictures.length > 0"
+				class="w-full [height:55svh] rounded-lg shadow-lg"
+				:style="{
+					backgroundImage: `url('${pictures[currentIndex].path}')`,
+					backgroundSize: 'cover',
+					backgroundPosition: 'center'
+				}"
+            >
+            <button
+                v-if="pictures.length > 1"
+                @click="prevImage"
+                class="absolute left-2 top-1/2 -translate-y-1/2 bg-white bg-opacity-70 rounded-full p-2 shadow cursor-pointer"
+                aria-label="Previous image"
+            >
+                ‹
+            </button>
+            <button
+                v-if="pictures.length > 1"
+                @click="nextImage"
+                class="absolute right-2 top-1/2 -translate-y-1/2 bg-white bg-opacity-70 rounded-full p-2 shadow cursor-pointer"
+                aria-label="Next image"
+            >
+                ›
+            </button>
+        </div>
+        <div v-if="pictures.length > 1" class="flex gap-2 mt-2 justify-center">
+            <span
+                v-for="(pic, idx) in pictures"
+                :key="idx"
+                class="w-3 h-3 rounded-full"
+                :class="currentIndex === idx ? 'bg-blue-600' : 'bg-gray-300'"
+            ></span>
+        </div>
+    </div>
+</div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+
+const props = defineProps(['pictures'])
+
+const pictures = computed(() => {
+    return props.pictures && props.pictures.length > 0
+        ? props.pictures
+        : [{ path: 'https://placehold.co/450x300' }]
+})
+
+const currentIndex = ref(0)
+
+function prevImage() {
+    currentIndex.value =
+        (currentIndex.value - 1 + pictures.value.length) % pictures.value.length
+}
+
+function nextImage() {
+    currentIndex.value = (currentIndex.value + 1) % pictures.value.length
+}
+</script>
--- resources/js/components/Pages/SingleDog.vue
+++ resources/js/components/Pages/SingleDog.vue
@@ -2,9 +2,7 @@
   <section class="px-4 md:px-8 py-11 mt-11">
     <h1 class="text-5xl md:text-7xl font-semibold text-left py-4 uppercase">{{ blogPost.dog_race.name }}</h1>
     <div class="w-full flex flex-col md:flex-row items-center justify-between gap-6 mt-8">
-      <div class="w-full md:w-1/2">
-        <img :src="pictures[0].path" :alt="'Chiot '+ blogPost.dog_race.name " class="w-full h-auto rounded-lg shadow-lg">
-      </div>
+      <Carroussel :pictures="pictures" />
       <div class="w-full md:w-1/2">
         <p class="text-2xl font-semibold text-left w-full">{{ blogPost.title }}</p>
         <p class="text-lg text-left w-full">{{  blogPost.dog_race.description }}</p>
@@ -19,17 +17,11 @@
 
 <script setup>
 import { ref } from 'vue'
+import Carroussel from '../Carroussel.vue';
 
 const blogPost = ref(window.pageData.blogPost)
 const dogRaces = ref(window.pageData.dogRaces)
 const pictures = ref(blogPost.value.pictures);
 
-if (!pictures.value || pictures.value.length === 0) {
-  pictures.value = [{ path: 'https://placehold.co/450x300' }];
-}
 
-console.log('Pictures array Lenght', pictures.value.length);
-console.log('Pictures array', pictures.value);
-
-console.log('BlogPost', blogPost.value);
 </script>
