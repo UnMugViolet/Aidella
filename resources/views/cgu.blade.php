66e678a9d3f1b6749cbe4b216ab0e42f976ea7dd
unmugviolet <jaguinpaul@gmail.com>
2025-06-09 21:35:12 +0200

    Fix typo

--- resources/views/cgu.blade.php
+++ resources/views/cgu.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Conditions Générales d\utilisation - Aidella')
+@section('title', 'Conditions Générales d\'utilisation - Aidella')
 
 @section('component', 'CGU')
 
