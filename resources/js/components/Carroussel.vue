053a20f4c5e1a99056af86196a247531104dbefa
unmugviolet <jaguinpaul@gmail.com>
2025-06-14 15:25:39 +0200

    Update style for single post and dog

--- resources/js/components/Carroussel.vue
+++ resources/js/components/Carroussel.vue
@@ -1,44 +1,30 @@
 <template>
-    <div class="w-full md:w-1/2 flex flex-col items-center">
+    <div class="w-full md:w-9/12 flex flex-col items-center">
         <div class="relative w-full">
-            <div
-				v-if="pictures.length > 0"
-				class="w-full [height:55svh] rounded-lg shadow-lg"
-                :style="{
-                    backgroundImage: pictures[currentIndex].path.startsWith('http')
-                        ? `url('${pictures[currentIndex].path}')`
-                        : `url('/${pictures[currentIndex].path}')`,
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center'
-                }"
-            >
-            <button
-                v-if="pictures.length > 1"
-                @click="prevImage"
-                class="absolute left-2 top-1/2 -translate-y-1/2 bg-white bg-opacity-70 rounded-full p-2 shadow cursor-pointer"
-                aria-label="Previous image"
-            >
-                ‹
-            </button>
-            <button
-                v-if="pictures.length > 1"
-                @click="nextImage"
-                class="absolute right-2 top-1/2 -translate-y-1/2 bg-white bg-opacity-70 rounded-full p-2 shadow cursor-pointer"
-                aria-label="Next image"
-            >
-                ›
-            </button>
-        </div>
-        <div v-if="pictures.length > 1" class="flex gap-2 mt-2 justify-center">
-            <span
-                v-for="(pic, idx) in pictures"
-                :key="idx"
-                class="w-3 h-3 rounded-full"
-                :class="currentIndex === idx ? 'bg-blue-600' : 'bg-gray-300'"
-            ></span>
+            <div v-if="pictures.length > 0" class="w-full [height:55svh] rounded-lg shadow-lg" :style="{
+                backgroundImage: pictures[currentIndex].path.startsWith('http')
+                    ? `url('${pictures[currentIndex].path}')`
+                    : `url('/${pictures[currentIndex].path}')`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center'
+            }">
+                <button v-if="pictures.length > 1" @click="prevImage"
+                    class="absolute left-2 top-1/2 -translate-y-1/2 bg-white bg-opacity-70 rounded-full p-2 shadow cursor-pointer"
+                    aria-label="Previous image">
+                    ‹
+                </button>
+                <button v-if="pictures.length > 1" @click="nextImage"
+                    class="absolute right-2 top-1/2 -translate-y-1/2 bg-white bg-opacity-70 rounded-full p-2 shadow cursor-pointer"
+                    aria-label="Next image">
+                    ›
+                </button>
+            </div>
+            <div v-if="pictures.length > 1" class="flex gap-2 mt-2 justify-center">
+                <span v-for="(pic, idx) in pictures" :key="idx" class="w-3 h-3 rounded-full"
+                    :class="currentIndex === idx ? 'bg-blue-600' : 'bg-gray-300'"></span>
+            </div>
         </div>
     </div>
-</div>
 </template>
 
 <script setup>
